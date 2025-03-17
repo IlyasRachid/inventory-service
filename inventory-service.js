678ca315c9155181e391da3ca6b614887303e297
iLvan <ilvan@gmail.com>
2025-03-17 11:22:47 +0000

    new

--- inventory-service.js
+++ inventory-service.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// some changes
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
 
 // Mock Inventory Data
 let inventory = {
-  'item1': { name: 'Item 1', quantity: 10 },
-  'item2': { name: 'Item 2', quantity: 5 }
+  item1: { name: "Item 1", quantity: 10 },
+  item2: { name: "Item 2", quantity: 5 },
 };
 
 app.use(bodyParser.json());
 
 // Health check endpoint
-app.get('/health', (req, res) => res.status(200).send('OK'));
-
-
+app.get("/health", (req, res) => res.status(200).send("OK"));
 
 // Unified inventory update endpoint
-app.post('/inventory/update', (req, res) => {
-    const { itemId, quantity } = req.body;
+app.post("/inventory/update", (req, res) => {
+  const { itemId, quantity } = req.body;
 
-    console.log('Request received from Order service');
-    
-    if (!inventory[itemId]) {
-        return res.status(404).json({ error: 'Item not found in inventory' });
-    }
+  console.log("Request received from Order service");
 
-    if (inventory[itemId].quantity < quantity) {
-        return res.status(400).json({ error: 'Insufficient stock' });
-        /*
+  if (!inventory[itemId]) {
+    return res.status(404).json({ error: "Item not found in inventory" });
+  }
+
+  if (inventory[itemId].quantity < quantity) {
+    return res.status(400).json({ error: "Insufficient stock" });
+    /*
         // Delay the response by 10 seconds
         setTimeout(() => {
             return res.status(400).json({ error: 'Insufficient stock' });
         }, 10000); // 10000 milliseconds = 10 seconds
         */
-    }
+  }
 
-    inventory[itemId].quantity -= quantity;
-    res.status(200).json({ message: 'Inventory updated successfully' });    
-    /*
+  inventory[itemId].quantity -= quantity;
+  res.status(200).json({ message: "Inventory updated successfully" });
+  /*
     // Delay the response by 10 seconds
     setTimeout(() => {
         res.status(200).json({ message: 'Inventory updated successfully' });
@@ -48,5 +47,5 @@ app.post('/inventory/update', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Inventory service running on port ${PORT}`);
+  console.log(`Inventory service running on port ${PORT}`);
 });
